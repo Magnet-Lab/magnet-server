85e6b47302ff895ff8399112e41bbc76cb363891
YehyeokBang <qkddpgur318@gmail.com>
2023-06-20 01:05:26 +0900

    [#2] OpenAPI config 추가

---
+++ src/main/java/com/magnet/magnet/global/config/OpenApiConfig.java
@@ -0,0 +1,41 @@
+package com.magnet.magnet.global.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("Magnet API Document")
+                .version("v0.0.1")
+                .description("Magnet API 설명서");
+
+        String authName = "JWT token";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(authName);
+        Components components = new Components()
+                .addSecuritySchemes(
+                        authName,
+                        new SecurityScheme()
+                                .name(authName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("Bearer")
+                                .bearerFormat("JWT")
+                                .description("accessToken 입력하세요. (Bearer 필요없음)")
+                );
+
+        return new OpenAPI()
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .info(info);
+    }
+
+}

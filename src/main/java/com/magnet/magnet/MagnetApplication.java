a01224beb18708eb10909928910a99ecc6fe1811
YehyeokBang <qkddpgur318@gmail.com>
2023-06-19 20:54:34 +0900

    [#1] 프로젝트 생성

---
+++ src/main/java/com/magnet/magnet/MagnetApplication.java
@@ -0,0 +1,13 @@
+package com.magnet.magnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagnetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magnet/magnet/MagnetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnet.magnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

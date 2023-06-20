3d158ef6151fc9fac121d296b3e0b6f834baba20
YehyeokBang <qkddpgur318@gmail.com>
2023-06-20 14:32:33 +0900

    [#5] CI 빌드 테스트

--- src/test/java/com/magnet/magnet/CITest.java
+++ src/test/java/com/magnet/magnet/CITest.java
@@ -2,11 +2,14 @@ package com.magnet.magnet;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class CITest {
 
     @Test
     void test() {
         System.out.println("CI test");
+        assertTrue(true);
     }
 
 }

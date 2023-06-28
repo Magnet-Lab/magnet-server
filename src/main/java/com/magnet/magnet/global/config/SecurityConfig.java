bb41eadf990096710c91c0dc7e71d7d73f2f0281
YehyeokBang <qkddpgur318@gmail.com>
2023-06-28 15:59:22 +0900

    [#8] Security Filter에 JWT Filter 추가

--- src/main/java/com/magnet/magnet/global/config/SecurityConfig.java
+++ src/main/java/com/magnet/magnet/global/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.magnet.magnet.global.config;
 
+import com.magnet.magnet.global.auth.jwt.JwtFilter;
+import com.magnet.magnet.global.auth.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -16,10 +21,13 @@ import java.util.List;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     @Value("${cors.allowed-origins}")
     String[] corsOrigins; // 허용할 Origin(요청 주소) 목록
+    private final TokenProvider tokenProvider;
+    private final RedisTemplate redisTemplate;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -32,10 +40,11 @@ public class SecurityConfig {
                 .logout(AbstractHttpConfigurer::disable) // logout 비활성화
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
-                                .requestMatchers("/", "/swagger-ui/**", "/api-docs/**").permitAll() // 누구나 접근 가능한 경로
+                                .requestMatchers("/", "/swagger-ui/**", "/api-docs/**", "/login/oauth2/**").permitAll() // 누구나 접근 가능한 경로
                                 .anyRequest().authenticated() // 나머지 경로는 인증 필요
                 )
                 .cors((cors) -> cors.configurationSource(configurationSource())) // cors 설정
+                .addFilterBefore(new JwtFilter(tokenProvider, redisTemplate), UsernamePasswordAuthenticationFilter.class) // JwtFilter 등록
                 .build();
     }
 

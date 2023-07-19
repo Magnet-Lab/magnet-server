5be6bb535dfd0986877a28b656652fdb14c36e1c
YehyeokBang <qkddpgur318@gmail.com>
2023-07-20 02:22:53 +0900

    [#31] 닉네임 설정 필드 추가

--- src/main/java/com/magnet/magnet/domain/club/dto/response/ResponseClub.java
+++ src/main/java/com/magnet/magnet/domain/club/dto/response/ResponseClub.java
@@ -26,6 +26,9 @@ public class ResponseClub {
     @Schema(description = "동아리내 나의 역할")
     private String myRole;
 
+    @Schema(description = "동아리내 나의 닉네임")
+    private String myNickname;
+
     @Schema(description = "동아리 생성일")
     private LocalDateTime createdDate;
 
